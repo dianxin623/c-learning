52eeac0c44722afc573c32a2dd17c523b817ea05
pioneer <278@pion.com>
2025-03-22 13:44:50 +0800

    犯得上反对

---
+++ ch1_04.cpp
@@ -0,0 +1,23 @@
+// created by 伍先锋
+// on 0319_25
+// 变量的使用、特征等
+
+#include <iostream>
+using namespace std;
+#include <windows.h>
+
+int main(){
+    //unsigned int age;
+    //age = 21;
+    //u_int fff= 55555;
+    //std::cout<<age<<'\n' <<'\t'<< fff << sizeof(3.14L);
+
+    char a[] = "fdsabg";
+    //const char *aaa = "abc";
+    string ccc = "fdsfdfdsfdsfdssd";
+    cout <<a<<'\n';
+    //cout << aaa<< '\n';
+    cout << ccc <<'\n';
+
+    return 0;
+}
---
+++ exe1.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+# define ALL 555
+
+int main(){
+    cout << "hfffffello world!!!" << 
+         34324325 << endl;
+    unsigned int a=-190;
+    cout<<a;
+    return 0;
+}
