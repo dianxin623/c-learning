d40fec416eb3beb8ac8dfe0fb8dd96f076a660e7
pioneer <278@pion.com>
2025-03-31 21:41:10 +0800

    学习了goto用法

---
+++ 跳转语句goto.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pion on 2025/3/31.
+//
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"1、"<<endl;
+    goto flag;
+    cout<<"2、"<<endl;
+    cout<<"3、"<<endl;
+    flag:
+    cout<<"4、"<<endl;
+}
